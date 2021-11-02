6855db02bec0f0f1106f0574af5bbb02ffc442f2
btw <@>
2021-11-02 21:29:34 +0800

    implement excel parser

--- AcademicAffairsToolkit/ExcelProcessor.cs
+++ AcademicAffairsToolkit/ExcelProcessor.cs
@@ -5,23 +5,53 @@ using NPOI.SS.UserModel;
 
 namespace AcademicAffairsToolkit
 {
-    static class ExcelProcessor
+    class InvigilateRecordEntry
     {
-        static void Read(string path, string password = "")
-        {
-            // todo: read according to policy
-            var workbook = WorkbookFactory.Create(path, password, true);
-        }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Subject { get; set; }
+        public string Department { get; set; }
+        public int Grade { get; set; }
+        public int ExamineeCount { get; set; }
+        public string Location { get; set; }
     }
 
-    class FileParsePolicy
+    class InvigilateFileParsePolicy
     {
+        public int Sheet { get; set; }
         public int DateColumn { get; set; }
         public int TimeColumn { get; set; }
+        public int DepartmentColumn { get; set; }
         public int SubjectColumn { get; set; }
-        public int InvigilatorColumn { get; set; }
         public int ExamineeCountColumn { get; set; }
-        public int CourseNameColumn { get; set; }
-        public string InvigilatorRegex { get; set; }
+        public int LocationColumn { get; set; }
+    }
+
+    static class ExcelProcessor
+    {
+        static IEnumerable<InvigilateRecordEntry> Read(string path, string password, InvigilateFileParsePolicy policy)
+        {
+            var workbook = WorkbookFactory.Create(path, password, true);
+            var sheet = workbook.GetSheetAt(policy.Sheet);
+
+            foreach (IRow row in sheet)
+            {
+                // todo: complete row-wise parsing
+                string dateString = row.GetCell(policy.DateColumn).StringCellValue;
+                string startTimeString = row.GetCell(policy.TimeColumn).StringCellValue;
+                string endTimeString = row.GetCell(policy.TimeColumn).StringCellValue;
+                _ = int.TryParse(row.GetCell(policy.ExamineeCountColumn).StringCellValue, out int examineeCount);
+
+                yield return new InvigilateRecordEntry
+                {
+                    Department = row.GetCell(policy.DepartmentColumn).StringCellValue,
+                    Subject = row.GetCell(policy.SubjectColumn).StringCellValue,
+                    StartTime = DateTime.Parse($"{dateString} {startTimeString}"),
+                    EndTime = DateTime.Parse($"{dateString} {endTimeString}"),
+                    Location = row.GetCell(policy.LocationColumn).StringCellValue,
+                    ExamineeCount = examineeCount
+                };
+            }
+        }
     }
 }
