0c071cacb3387e505ff231863b3e748afb94c88f
btw <@>
2021-11-02 21:38:01 +0800

    add session class

---
+++ AcademicAffairsToolkit/Session.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace AcademicAffairsToolkit
+{
+    enum SessionStatus
+    {
+        FileNotOpen,
+        FileOpened,
+        Evaluated,
+        Invalid
+    }
+
+    class Session
+    {
+        public SessionStatus Status { get; set; }
+        public IEnumerable<InvigilateRecordEntry> InvigilateRecords { get; set; }
+    }
+}
