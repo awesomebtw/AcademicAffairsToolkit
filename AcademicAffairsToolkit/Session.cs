475b22deaac35d67436bb7ac66875f518ddafef3
btw <@>
2021-11-05 12:53:09 +0800

    use static property binding syntax for file views

--- AcademicAffairsToolkit/Session.cs
+++ AcademicAffairsToolkit/Session.cs
@@ -6,16 +6,16 @@ namespace AcademicAffairsToolkit
 {
     static class Session
     {
-        public static InvigilateFileParsePolicy InvigilateFilePolicy = InvigilateFileParsePolicy.DefaultPolicy;
+        public static InvigilateFileParsePolicy InvigilateFilePolicy { get; set; } = InvigilateFileParsePolicy.DefaultPolicy;
 
-        public static TROfficeFileParsePolicy TROfficeFilePolicy = TROfficeFileParsePolicy.DefaultPolicy;
+        public static TROfficeFileParsePolicy TROfficeFilePolicy { get; set; } = TROfficeFileParsePolicy.DefaultPolicy;
 
-        public static ObservableCollection<InvigilateRecordEntry> InvigilateRecords = null;
+        public static ObservableCollection<InvigilateRecordEntry> InvigilateRecords { get; set; }
 
-        public static ObservableCollection<TROfficeRecordEntry> TROffices = null;
+        public static ObservableCollection<TROfficeRecordEntry> TROffices { get; set; }
 
         // todo: store arrangement data
-        public static IEnumerable<object> Arrangements;
+        public static IEnumerable<object> Arrangements { get; set; }
 
         public static bool AnyFileLoaded() =>
             InvigilateRecords != null || TROffices != null;
