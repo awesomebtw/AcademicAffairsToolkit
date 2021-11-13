db6a07ca669238232dd110926a24cc615015ea19
btw <@>
2021-11-13 22:36:06 +0800

    add initial implementation of fitness function; comments for some functions

--- AcademicAffairsToolkit/InvigilateArrangement.cs
+++ AcademicAffairsToolkit/InvigilateArrangement.cs
@@ -12,9 +12,9 @@ namespace AcademicAffairsToolkit
 
     class GeneticAlgorithmScheduler : InvigilateArrangement
     {
-        private static double CrossoverProbability = 0.75;
+        private static readonly double crossoverProbability = 0.75;
 
-        private static double MutationProbobility = 0.1;
+        private static readonly double mutationProbobility = 0.1;
 
         public IntervalTree<DateTime, InvigilateRecordEntry> InvigilateRecords { get; private set; }
 
@@ -75,12 +75,12 @@ namespace AcademicAffairsToolkit
         }
 
         /// <summary>
-        /// generate a random permutation of arrangement
+        /// generate a random permutation of arrangement regardless of user-defined constraints
         /// </summary>
         /// <returns>a tuple with office, invigilate task and people needed</returns>
         private IEnumerable<Tuple<TROfficeRecordEntry, InvigilateRecordEntry, int>> GeneratePermutation()
         {
-            var invigilateList = InvigilateRecords.Select(p => (record: p.Value, peopleNeeded: GetInvigilatePersonCount(p.Value.ExamineeCount))).ToList();
+            var invigilateList = InvigilateRecords.Select(p => (p.Value, GetInvigilatePersonCount(p.Value.ExamineeCount))).ToList();
             var trOfficeList = new List<TROfficeRecordEntry>(TROfficeRecords);
             var trOfficeRemaining = TROfficeRecords.Select(p => p.PeopleCount).ToList();
 
@@ -89,20 +89,28 @@ namespace AcademicAffairsToolkit
             Random random = new Random();
 
             // assign for each invigilate task
-            for (int i = 0; i < invigilateList.Count(); i++)
+            for (int i = 0; i < invigilateList.Count; i++)
             {
                 var (iRecord, peopleNeeded) = invigilateList[i];
 
                 // ensure the selected office has enough people
                 // by picking a random index and check if people is enough
+                // to prevent endless loop when there's no office suitable,
+                // a variable is added here to record the initial index
                 int j = random.Next(trOfficeList.Count);
+                int k = j;
                 while (trOfficeRemaining[j] < peopleNeeded)
+                {
                     j = (j + 1) % trOfficeList.Count;
+                    if (j == k)
+                        yield break; // todo: return with a fail flag
+                }
 
                 yield return Tuple.Create(trOfficeList[j], iRecord, peopleNeeded);
 
-                trOfficeRemaining[j] -= peopleNeeded; // subtract 
-                if (trOfficeRemaining[j] == 0)
+                // update remaining people list after assignment
+                trOfficeRemaining[j] -= peopleNeeded;
+                if (trOfficeRemaining[j] < 2)
                 {
                     trOfficeList.RemoveAt(j);
                     trOfficeRemaining.RemoveAt(j);
@@ -110,6 +118,11 @@ namespace AcademicAffairsToolkit
             }
         }
 
+        /// <summary>
+        /// randomly generate a population (a group of arrangement) regardless of user-defined constraints
+        /// </summary>
+        /// <param name="count">size of the generated popullation</param>
+        /// <returns>generated population</returns>
         private IEnumerable<IEnumerable<Tuple<TROfficeRecordEntry, InvigilateRecordEntry, int>>> GeneratePopulation(int count)
         {
             for (int i = 0; i < count; i++)
@@ -118,15 +131,42 @@ namespace AcademicAffairsToolkit
             }
         }
 
+        /// <summary>
+        /// evaluate the chromosome's fitness according to constraints, etc
+        /// </summary>
+        /// <param name="chromosome">the chromosome to be evaluated</param>
+        /// <returns>fitness of given chromosome</returns>
         private int GetFitness(IEnumerable<Tuple<TROfficeRecordEntry, InvigilateRecordEntry, int>> chromosome)
         {
-            // todo: fitness funtion evaluation
-            return 0;
+            const int constarintViolationFactor = 150;
+            const int arrangeFactor = 200;
+
+            int fitness = 0;
+
+            // see how many constraints are violated in population size times log(n)+m time
+            fitness -= constarintViolationFactor * chromosome.Count(
+                p => Constraints.Query(p.Item2.StartTime, p.Item2.EndTime).Contains(p.Item1));
+
+            // check if people assigned is more than an office's people count
+            fitness -= arrangeFactor * chromosome.GroupBy(p => p.Item1, p => p.Item3).Where(p => p.Key.PeopleCount < p.Sum()).Count();
+
+            // todo: check if there's any time overlap
+
+            // check if the arrangement is evenly spaced by evaluating the variance of time
+            var group = chromosome.GroupBy(p => p.Item1, p => ((DateTimeOffset)p.Item2.StartTime).ToUnixTimeSeconds());
+            foreach (var item in group)
+            {
+                var avg = item.Average();
+                fitness += (int)(item.Sum(p => Math.Pow(p - avg, 2)) / item.Count());
+            }
+
+            return fitness;
         }
 
-        private void ApplyCrossover()
+        private IEnumerable<Tuple<TROfficeRecordEntry, InvigilateRecordEntry, int>> ApplyCrossover(IEnumerable<Tuple<TROfficeRecordEntry, InvigilateRecordEntry, int>> left, IEnumerable<Tuple<TROfficeRecordEntry, InvigilateRecordEntry, int>> right)
         {
-            // todo: 
+            // todo: do crossover
+            yield break;
         }
 
         private void Mutate()
@@ -141,10 +181,10 @@ namespace AcademicAffairsToolkit
             var population = GeneratePopulation(PopulationCount);
             for (int i = 0; i < Iterations; i++)
             {
-                var fitness = population.Select(p => GetFitness(p));
-                if (random.NextDouble() < CrossoverProbability)
-                    ApplyCrossover();
-                if (random.NextDouble() < MutationProbobility)
+                var fitness = population.OrderByDescending(p => GetFitness(p));
+                if (random.NextDouble() < crossoverProbability)
+                    ApplyCrossover(population.ElementAt(0), population.ElementAt(1));
+                if (random.NextDouble() < mutationProbobility)
                     Mutate();
                 // replace the old population with new one
             }
