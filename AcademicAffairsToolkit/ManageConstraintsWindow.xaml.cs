0b8cb0d4780b4a9d0a39e2a744a8acdbae85c076
btw <@>
2021-11-14 14:19:37 +0800

    initial implementation of adding and deleting constraint functionality

--- AcademicAffairsToolkit/ManageConstraintsWindow.xaml.cs
+++ AcademicAffairsToolkit/ManageConstraintsWindow.xaml.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace AcademicAffairsToolkit
 {
@@ -20,6 +10,31 @@ namespace AcademicAffairsToolkit
         public ManageConstraintsWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddButtonClick(object sender, RoutedEventArgs e)
+        {
+            // properties in datetime are read-only and can't bind to datetime
+            // so we have to update it manually
+            if (trOfficePicker.SelectedIndex != -1)
+                Session.Constraints.Add(new InvigilateConstraint(
+                    fromDatePicker.DisplayDate.Date.AddHours(fromHourPicker.Value).AddMinutes(fromMinutePicker.Value),
+                    toDatePicker.DisplayDate.Date.AddHours(toHourPicker.Value).AddMinutes(toMinutePicker.Value),
+                    trOfficePicker.SelectedItem as TROfficeRecordEntry));
+            else
+                MessageBox.Show("Please select an office before adding constraint.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void RemoveButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (itemsGallery.SelectedIndex != -1)
+                Session.Constraints.RemoveAt(itemsGallery.SelectedIndex);
+        }
+
+        private void TimePickerValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // properties in datetime are read-only and can't bind to datetime
+            // so we have to update it manually
         }
     }
 }
