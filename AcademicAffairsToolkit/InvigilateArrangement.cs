ca0f90bf978bc49c18c037c8d3e7537b1376377a
btw <@>
2021-11-15 22:38:50 +0800

    use array for chromosome

--- AcademicAffairsToolkit/InvigilateArrangement.cs
+++ AcademicAffairsToolkit/InvigilateArrangement.cs
@@ -16,7 +16,9 @@ namespace AcademicAffairsToolkit
 
         private static readonly double mutationProbobility = 0.1;
 
-        public IntervalTree<DateTime, InvigilateRecordEntry> InvigilateRecords { get; private set; }
+        public InvigilateRecordEntry[] InvigilateRecords { get; private set; }
+
+        public int[] PeopleNeeded { get; set; }
 
         public IntervalTree<DateTime, TROfficeRecordEntry> Constraints { get; private set; }
 
@@ -26,19 +28,26 @@ namespace AcademicAffairsToolkit
 
         public int PopulationCount { get; set; }
 
-        // todo: result type
-        public List<(TROfficeRecordEntry, InvigilateRecordEntry, int personCount)> Result { get; private set; }
+        public IEnumerable<(TROfficeRecordEntry, InvigilateRecordEntry, int personCount)> Result { get; private set; }
 
+        /// <summary>
+        /// construct a new object for invigilate arrangement
+        /// </summary>
+        /// <param name="invigilateRecords"></param>
+        /// <param name="trOfficeRecords">teaching and researching offices to be assigned</param>
+        /// <param name="constraints">time constraints to be applied when evaluating arrangement</param>
+        /// <param name="iterations">maximum iteration for arrangement</param>
         public GeneticAlgorithmScheduler(IEnumerable<InvigilateRecordEntry> invigilateRecords, IEnumerable<TROfficeRecordEntry> trOfficeRecords, IEnumerable<InvigilateConstraint> constraints, int iterations)
         {
             TROfficeRecords = trOfficeRecords;
             Iterations = iterations;
 
-            foreach (var record in invigilateRecords)
-            {
-                InvigilateRecords.Add(record.StartTime, record.EndTime, record);
-            }
+            // sort invigilate records by time for convenience
+            // no need to use SortedList
+            InvigilateRecords = invigilateRecords.OrderBy(p => p.StartTime).ThenBy(p => p.EndTime).ToArray();
+            PeopleNeeded = invigilateRecords.Select(p => GetInvigilatePersonCount(p.ExamineeCount)).ToArray();
 
+            // use interval tree for searching constraints to reduce time complexity
             Constraints = new IntervalTree<DateTime, TROfficeRecordEntry>();
             foreach (var constraint in constraints)
             {
@@ -46,6 +55,12 @@ namespace AcademicAffairsToolkit
             }
         }
 
+        /// <summary>
+        /// get invigilate person needed for an exam according to requirement
+        /// </summary>
+        /// <param name="students">examinee count of an exam</param>
+        /// <returns>count of person needed</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         private static int GetInvigilatePersonCount(int students)
         {
             if (students < 0)
@@ -77,39 +92,36 @@ namespace AcademicAffairsToolkit
         /// <summary>
         /// generate a random permutation of arrangement regardless of user-defined constraints
         /// </summary>
-        /// <returns>a tuple with office, invigilate task and people needed</returns>
-        private IEnumerable<Tuple<TROfficeRecordEntry, InvigilateRecordEntry, int>> GeneratePermutation()
+        /// <returns>An array of tuple with office and people needed,
+        /// whose length is InvigilateRecords.Length and
+        /// invigilate task at InvigilateRecords[i] is assigned to chromosome[i].</returns>
+        private IEnumerable<Tuple<TROfficeRecordEntry, int>> GenerateChromosome()
         {
-            var invigilateList = InvigilateRecords.Select(p => (p.Value, GetInvigilatePersonCount(p.Value.ExamineeCount))).ToList();
             var trOfficeList = new List<TROfficeRecordEntry>(TROfficeRecords);
             var trOfficeRemaining = TROfficeRecords.Select(p => p.PeopleCount).ToList();
-
-            Shuffle(ref invigilateList);
 
             Random random = new Random();
 
             // assign for each invigilate task
-            for (int i = 0; i < invigilateList.Count; i++)
+            for (int i = 0; i < InvigilateRecords.Length; i++)
             {
-                var (iRecord, peopleNeeded) = invigilateList[i];
-
                 // ensure the selected office has enough people
                 // by picking a random index and check if people is enough
                 // to prevent endless loop when there's no office suitable,
                 // a variable is added here to record the initial index
                 int j = random.Next(trOfficeList.Count);
                 int k = j;
-                while (trOfficeRemaining[j] < peopleNeeded)
+                while (trOfficeRemaining[j] < PeopleNeeded[i])
                 {
                     j = (j + 1) % trOfficeList.Count;
                     if (j == k)
-                        yield break; // todo: return with a fail flag
+                        throw new ApplicationException("available person is not sufficient for arrangement");
                 }
 
-                yield return Tuple.Create(trOfficeList[j], iRecord, peopleNeeded);
+                yield return Tuple.Create(trOfficeList[j], PeopleNeeded[i]);
 
                 // update remaining people list after assignment
-                trOfficeRemaining[j] -= peopleNeeded;
+                trOfficeRemaining[j] -= PeopleNeeded[i];
                 if (trOfficeRemaining[j] < 2)
                 {
                     trOfficeList.RemoveAt(j);
@@ -123,11 +135,11 @@ namespace AcademicAffairsToolkit
         /// </summary>
         /// <param name="count">size of the generated popullation</param>
         /// <returns>generated population</returns>
-        private IEnumerable<IEnumerable<Tuple<TROfficeRecordEntry, InvigilateRecordEntry, int>>> GeneratePopulation(int count)
+        private IEnumerable<Tuple<TROfficeRecordEntry, int>[]> GenerateInitialPopulation(int count)
         {
             for (int i = 0; i < count; i++)
             {
-                yield return GeneratePermutation();
+                yield return GenerateChromosome().ToArray();
             }
         }
 
@@ -136,34 +148,43 @@ namespace AcademicAffairsToolkit
         /// </summary>
         /// <param name="chromosome">the chromosome to be evaluated</param>
         /// <returns>fitness of given chromosome</returns>
-        private int GetFitness(IEnumerable<Tuple<TROfficeRecordEntry, InvigilateRecordEntry, int>> chromosome)
+        private int GetFitness(in Tuple<TROfficeRecordEntry, int>[] chromosome)
         {
             const int constarintViolationFactor = 150;
             const int arrangeFactor = 200;
+            const int distributionFactor = 100;
 
             int fitness = 0;
 
             // see how many constraints are violated in population size times log(n)+m time
-            fitness -= constarintViolationFactor * chromosome.Count(
-                p => Constraints.Query(p.Item2.StartTime, p.Item2.EndTime).Contains(p.Item1));
+            for (int i = 0; i < InvigilateRecords.Length; i++)
+            {
+                var record = InvigilateRecords[i];
+                if (Constraints.Query(record.StartTime, record.EndTime).Contains(chromosome[i].Item1))
+                    fitness -= constarintViolationFactor;
+            }
 
             // check if people assigned is more than an office's people count
-            fitness -= arrangeFactor * chromosome.GroupBy(p => p.Item1, p => p.Item3).Where(p => p.Key.PeopleCount < p.Sum()).Count();
+            fitness -= arrangeFactor * chromosome.GroupBy(p => p.Item1, p => p.Item2)
+                .Count(p => p.Key.PeopleCount < p.Sum());
 
-            // todo: check if there's any time overlap
-
-            // check if the arrangement is evenly spaced by evaluating the variance of time
-            var group = chromosome.GroupBy(p => p.Item1, p => ((DateTimeOffset)p.Item2.StartTime).ToUnixTimeSeconds());
-            foreach (var item in group)
+            // since the invigilate record is sorted by start and end time,
+            // we can check if the arrangement is evenly spaced by evaluating how many repetitions in the chromosome
+            List<int> repetitionItems = new List<int>(chromosome.Length);
+            for (int i = 1; i < chromosome.Length; i++)
             {
-                var avg = item.Average();
-                fitness += (int)(item.Sum(p => Math.Pow(p - avg, 2)) / item.Count());
+                if (chromosome[i - 1].Item1 == chromosome[i].Item1)
+                    repetitionItems[^1]++;
+                else
+                    repetitionItems.Add(0);
             }
+
+            fitness -= distributionFactor * repetitionItems.Sum();
 
             return fitness;
         }
 
-        private IEnumerable<Tuple<TROfficeRecordEntry, InvigilateRecordEntry, int>> ApplyCrossover(IEnumerable<Tuple<TROfficeRecordEntry, InvigilateRecordEntry, int>> left, IEnumerable<Tuple<TROfficeRecordEntry, InvigilateRecordEntry, int>> right)
+        private IEnumerable<Tuple<TROfficeRecordEntry, int>> ApplyCrossover(IEnumerable<Tuple<TROfficeRecordEntry, int>> left, IEnumerable<Tuple<TROfficeRecordEntry, int>> right)
         {
             // todo: do crossover
             yield break;
@@ -178,7 +199,7 @@ namespace AcademicAffairsToolkit
         {
             // todo: algorithm
             Random random = new Random();
-            var population = GeneratePopulation(PopulationCount);
+            var population = GenerateInitialPopulation(PopulationCount);
             for (int i = 0; i < Iterations; i++)
             {
                 var fitness = population.OrderByDescending(p => GetFitness(p));
