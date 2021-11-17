bc0f3d8eaa0ac5fff91a0616476bf6e5db9a4dca
btw <@>
2021-11-17 23:15:23 +0800

    use a class to replace tuple for arrangement results

---
+++ AcademicAffairsToolkit/ArrangementResultEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcademicAffairsToolkit
+{
+    class ArrangementResultEntry
+    {
+        public InvigilateRecordEntry InvigilateRecord { get; set; }
+
+        public TROfficeRecordEntry TROfficeRecord { get; set; }
+
+        public int PeopleNeeded { get; set; }
+
+        public ArrangementResultEntry(InvigilateRecordEntry invigilateRecord, TROfficeRecordEntry trOfficeRecord, int peopleNeeded)
+        {
+            InvigilateRecord = invigilateRecord;
+            TROfficeRecord = trOfficeRecord;
+            PeopleNeeded = peopleNeeded;
+        }
+    }
+}
