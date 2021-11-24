d8b2bb8ab27fa8f9fa3e77cd8839f18cd953b67e
btw <@>
2021-11-24 22:05:27 +0800

    add datetimepicker range check for manage constraint window

--- AcademicAffairsToolkit/ManageConstraintsWindow.xaml.cs
+++ AcademicAffairsToolkit/ManageConstraintsWindow.xaml.cs
@@ -17,5 +17,18 @@ namespace AcademicAffairsToolkit
             if (itemsGallery.SelectedIndex != -1)
                 Session.Constraints.RemoveAt(itemsGallery.SelectedIndex);
         }
+
+        private void DateTimePickerValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (fromDateTimePicker != null && toDateTimePicker != null &&
+                fromDateTimePicker.Value >= toDateTimePicker.Value)
+            {
+                MessageBox.Show(Resource.EndTimeEarlierThanStartTimeErrorTip, Resource.Error,
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+
+                (sender as Xceed.Wpf.Toolkit.DateTimeUpDown).Value = (System.DateTime)e.OldValue;
+                e.Handled = true;
+            }
+        }
     }
 }
